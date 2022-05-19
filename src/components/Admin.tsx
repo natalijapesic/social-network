149ecf00d8ee694fd38fe58fa5c2dfabffe49761
natalija <natalijapesic@elfak.rs>
2022-05-19 16:47:39 +0200

    AdminPage

---
+++ src/components/Admin.tsx
@@ -0,0 +1,41 @@
+import {
+  getPostsError,
+  getPostsStatus,
+  selectAllPosts,
+} from "../features/posts/postSlice";
+import { useAppSelector } from "../stores/hooks";
+import AdminPost from "./AdminPost";
+
+const headerInfo = ['#', 'Title', 'Image', 'Description', 'Author name', 'Date', 'Likes', 'Delete'];
+
+const Admin: React.FC = () => {
+  let posts = useAppSelector(selectAllPosts);
+  let postStatus = useAppSelector(getPostsStatus);
+  let error = useAppSelector(getPostsError);
+
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    content = posts.map((post, index) => <AdminPost key={index} {...post} />);
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <div className="pt-10 relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                {headerInfo.map((info, index) => <th key={index} className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">{info}</th>)}
+            </tr>
+        </thead>
+        <tbody>
+            {content}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/components/AdminPost.tsx
@@ -0,0 +1,49 @@
+import { deletePost } from "../features/posts/postSlice";
+import { useAppDispatch } from "../stores/hooks";
+import Button from "./Button";
+import { ButtonStyle } from "./types";
+
+interface IProps {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+  authorName: string;
+  date: string;
+  likes: number;
+  usersLikes: number[];
+  userId: number;
+}
+
+const AdminPost: React.FC<IProps> = (post: IProps) => {
+  const dispatch = useAppDispatch();
+
+  const onDelete = () => {
+    if (window.confirm("Are you sure you want to delete this post?"))
+      dispatch(deletePost(post.id));
+  };
+
+  return (
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={post.id}>
+        <td>{post.id}</td>
+        <td>{post.title}</td>
+        <td>{post.image}</td>
+        <td>{post.description}</td>
+        <td>{post.authorName}</td>
+        <td>{post.date}</td>
+        <td>{post.likes}</td>
+    <td>
+        <Button
+        value={post.id}
+        onClick={onDelete}
+        type="button"
+        buttonStyle={ButtonStyle.redRound}
+        message="X"
+        disabled={false}
+        />
+    </td>
+    </tr>
+  );
+};
+
+export default AdminPost;
